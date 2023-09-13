b295c5ed180e7748d8001c01861a4874218880a5
Mbabu19 <136243767+Mbabu19@users.noreply.github.com>
2023-09-13 15:39:01 +0530

    mongodb.js

--- form validation/src/mongodb.js
+++ form validation/src/mongodb.js
@@ -1,38 +1,3 @@
-// const mongoose=require('mongoose')
-
-// mongoose.connect("mongodb+srv://mbabu2001:1234@cluster0.5ocvqly.mongodb.net/?retryWrites=true&w=majority/LoginSignup")
-// .then(()=>{
-//     console.log("mongodb connected");
-// })
-// .catch(()=>{
-//     console.log("failed to connect");
-// })
-// const LoginSchema = new mongoose.Schema({
-//     username:{
-//         type:String,
-//         require:true,
-//     },
-//     email:{
-//         type:String,
-//         require:true,
-//     },
-//     password:{
-//         type:String,
-//         require:true,
-//     },
-//     cpassword:{
-//         type:String,
-//         require:true,
-//     },
-
-    
-// })
-// const collection =mongoose.model("LoginSignup",LoginSchema)
-
-// module.expert=collection
-
-
-
 const mongoose=require("mongoose")
 
 mongoose.connect("mongodb://127.0.0.1:27017/LoginSignup")
